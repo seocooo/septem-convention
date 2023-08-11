45cbc91d0d6216d585c5b2c5f9c3b87367c9cd8e
beomseok <beomseok627@septem-ai.com>
2023-08-11 16:51:57 +0900

    23-08-11 재작업

--- project/pub/com/js/common.js
+++ project/pub/com/js/common.js
@@ -1,63 +1,91 @@
-;(function(window, $) {
+(function (window, $) {
+  // 활성화시 사용될 클레스 명.
+  const actived = "ng-active";
 
-    // 활성화시 사용될 클레스 명.
-    const actived = 'ng-active';
+  // 초기 화면 (documnet가 최초 로드 후 동작될 ui component )
+  const componentUI = {
+    tabs: function () {
+      const $tabs = $(this);
+      const $tabNav = $tabs.find(">.ng_tabs-nav");
+      const $tabList = $tabNav.find(">.ng_tabs-list");
+      const $tabBtns = $tabNav.find(".ng_tabs-item");
+      const $tabPanels = $tabNav.find("+.ng_tabs-container>.ng_tabs-panel");
+      const $tabTarget = $tabList.get(0) ? $tabList : $tabBtns;
+      const thisCurrentIndex = Math.max(
+        $tabTarget.filter(`.${actived}`).index(),
+        0
+      );
 
-    // 초기 화면 (documnet가 최초 로드 후 동작될 ui component )
-    const componentUI = {
-        tabs: function () {
-            const $tabs = $(this);
-            const $tabNav = $tabs.find('>.ng_tabs-nav');
-            const $tabList = $tabNav.find('>.ng_tabs-list');
-            const $tabBtns = $tabNav.find('.ng_tabs-item');
-            const $tabPanels = $tabNav.find('+.ng_tabs-container>.ng_tabs-panel');
-            const $tabTarget = $tabList.get(0) ? $tabList : $tabBtns;
-            const thisCurrentIndex = Math.max($tabTarget.filter(`.${actived}`).index(), 0);
-
-            const tabClick = function(forced) {
-                const currentIndex = $tabBtns.index(this);
-                if ( !$tabTarget.eq(currentIndex).hasClass(actived) || forced ) {
-                    $tabTarget.removeClass(actived).eq(currentIndex).addClass(actived);
-                    $tabPanels.get(0) && $tabPanels.removeClass(actived).eq(currentIndex).addClass(actived);
-                }
-                return false;
-            };
-
-            $tabBtns.off('.ng-tabs').on('click.ng-tabs', tabClick);
-            $tabBtns.eq(thisCurrentIndex).trigger('click', true);
+      const tabClick = function (forced) {
+        const currentIndex = $tabBtns.index(this);
+        if (!$tabTarget.eq(currentIndex).hasClass(actived) || forced) {
+          $tabTarget.removeClass(actived).eq(currentIndex).addClass(actived);
+          $tabPanels.get(0) &&
+            $tabPanels.removeClass(actived).eq(currentIndex).addClass(actived);
         }
+        return false;
+      };
+
+      $tabBtns.off(".ng-tabs").on("click.ng-tabs", tabClick);
+      $tabBtns.eq(thisCurrentIndex).trigger("click", true);
+    },
+  };
+
+  const App = (function () {
+    const _init = function () {
+        window.addEventListener("DOMContentLoaded", this.welcome.bind(this));
+        window.addEventListener("resize", this.resize.bind(this));
+        window.addEventListener("scroll", this.scroll.bind(this));
+        return this;
+      },
+      _hello = function () {
+        // for(const properties in componentUI) {
+        //     typeof componentUI[properties] == 'function' && componentUI[properties].apply(this);
+        // }
+
+        const $tabs = $(".ng_tabs");
+        $tabs.each(componentUI.tabs);
+
+        window.$App = this;
+      },
+      _scroll = function () {},
+      _resize = function () {};
+    return {
+      init: _init,
+      resize: _resize,
+      scroll: _scroll,
+      welcome: _hello,
     };
-
-    const App = (function() {
-        const _init = function() {
-            window.addEventListener('DOMContentLoaded', this.welcome.bind(this));
-            window.addEventListener('resize', this.resize.bind(this));
-            window.addEventListener('scroll', this.scroll.bind(this));
-            return this;
-        },
-        _hello = function() {
-            // for(const properties in componentUI) {
-            //     typeof componentUI[properties] == 'function' && componentUI[properties].apply(this);
-            // }
-
-            const $tabs = $('.ng_tabs');
-            $tabs.each(componentUI.tabs);
-
-            window.$App = this;
-        },
-        _scroll = function() {
-
-        },
-        _resize = function() {
-
-        }
-        ;
-
-        return {
-            init: _init,
-            resize: _resize,
-            scroll: _scroll,
-            welcome: _hello
-        }
-    })().init();
+  })().init();
 })(window, jQuery);
+
+// 툴팁
+$(function () {
+  const bodyW = $(document.body).width();
+  const tooltipCont = $(".tooltip .tooltip-cont");
+  const tootipFull = $(".tooltip.js_tooltip-full .tooltip-cont");
+  const tooltipW = Math.floor(bodyW / 2);
+  const tooltipMargin = 40;
+
+  tooltipCont.css("width", tooltipW + "px");
+  tootipFull.css("width", bodyW - tooltipMargin + "px");
+});
+
+// 팝업
+$(function () {
+  $(".btn-modal").click(function () {
+    const targetPopup = $(this).attr("aria-controls");
+    $("#" + targetPopup)
+      .show()
+      .attr("aria-hidden", "false");
+    // $("#popupDim").show();
+  });
+
+  $(".btn-close, .modal-dim").click(function () {
+    $(".modal").hide().attr("aria-hidden", "true");
+  });
+
+  $(".modal").click(function (event) {
+    event.stopPropagation(); // 팝업 내부를 클릭하면 닫히지 않도록 중지
+  });
+});
--- project/readme/project.md
+++ project/readme/project.md
@@ -3,68 +3,100 @@
 ## 1. 자주 쓰는 네이밍 규칙 만들기
 
 1. 예시
-	```css
-	.modal {}
-	.modal-inner {}
-	.modal-araa {}
-	.modal-header {}
-	.modal-title {}
-	.modal-desc {}
-	.modal-container {}
-	.modal-content-title {}
-	.modal-content-desc {}
-	.modal-content {}
-	.modal-footer {}
-	.modal-btn-wrap {}
-	```
 
-	### 1.1 하위뎁스 사용시
-	`-inner`, `-wrap`, `-area`, `-header`, `-container`, `-content`, `-footer`,
-	`-group`, `-item`, `-list`, `-box`, `-sub`, `-side`, `-title`, `-desc`, `-cell`
-	`-info`, `-data`, `-link`, `-label`, `-unit`
-	
-	---
+   ```css
+   .modal {
+   }
+   .modal-inner {
+   }
+   .modal-araa {
+   }
+   .modal-header {
+   }
+   .modal-title {
+   }
+   .modal-desc {
+   }
+   .modal-container {
+   }
+   .modal-content-title {
+   }
+   .modal-content-desc {
+   }
+   .modal-content {
+   }
+   .modal-footer {
+   }
+   .modal-btn-wrap {
+   }
+   ```
 
-	### 1.2 속상 값 적용시
-	```css
-	 // st - Style Type 
-	.table {}
-	.table.st_borderless {}
-	.table.st_striped {}
+   ### 1.1 하위뎁스 사용시
 
-	.text-list {}
-	.text-list.st_dot {}
-	.text-list.st_dash {}
+   `-inner`, `-wrap`, `-area`, `-header`, `-container`, `-content`, `-footer`,
+   `-group`, `-item`, `-list`, `-box`, `-sub`, `-side`, `-title`, `-desc`, `-cell`
+   `-info`, `-data`, `-link`, `-label`, `-unit`
 
-	// cs - Change Status
-	.modal.cs_show {}
-	.radio.cs_active {}
-	.input.cs_disabled {}
+   ***
 
-	// css animation
-	.modal.ca_fade-in {}
-	.modal.ca_slidedown {}
+   ### 1.2 속상 값 적용시
 
-	// js control
-	.btn.js_btn-modal {}
-	.btn.js_btn-modal-close {}
-	.accodien.js-accordion {}
-	.accodien.js-accordion {}
-	```
+   ```css
+   // st - Style Type
+   .table {
+   }
+   .table.st_borderless {
+   }
+   .table.st_striped {
+   }
 
-	> 가급적 접두어를 붙여 상황에 맞는 값을 적용한다.
-	> 1. `st_[name]` : name 부분에는 번호가 아닌 의미있는 값을 적용한다.
-	> 2. `cs_[value]` : 변경되는 상태값을 의미한다.
-	> 
+   .text-list {
+   }
+   .text-list.st_dot {
+   }
+   .text-list.st_dash {
+   }
 
-	---
-	### 1.3 별도 속성 값 적용시
-	```
-	.tc {}
-	.bg-primary {}
-	.cl-primary {}
-	.dp-flex {}
-	```
-	> `emmit` 을 이용한 약어 사용으로 추가 속성을 지정한다.
-	> `bg-primary`, `tc`, `cl-primary`, `dp-flex`
+   // cs - Change Status
+   .modal.cs_show {
+   }
+   .radio.cs_active {
+   }
+   .input.cs_disabled {
+   }
 
+   // css animation
+   .modal.ani_fade-in {
+   }
+   .modal.ani_slidedown {
+   }
+
+   // js control
+   .btn.js_btn-modal {
+   }
+   .btn.js_btn-modal-close {
+   }
+   .accodien.js_accordion {
+   }
+   .accodien.js_accordion {
+   }
+   ```
+
+   > 가급적 접두어를 붙여 상황에 맞는 값을 적용한다.
+   >
+   > 1. `st_[name]` : name 부분에는 번호가 아닌 의미있는 값을 적용한다.
+   > 2. `cs_[value]` : 변경되는 상태값을 의미한다.
+
+   ***
+
+   ### 1.3 별도 속성 값 적용시
+
+   ```
+   .ta-center {}
+   .bg-primary {}
+   .c-primary {}
+   .dp-flex {}
+   ```
+
+   > `emmet` 을 이용한 약어 사용으로 추가 속성을 지정한다.
+   > `bg-primary`, `ta-center`, `c-primary`, `dp-flex`
